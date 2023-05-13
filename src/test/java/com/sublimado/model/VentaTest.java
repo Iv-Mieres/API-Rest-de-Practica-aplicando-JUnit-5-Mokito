db85b826f162e5c544d589c0beeacb9bb7ae4783
Ivan Mieres <mieres.iv@gmail.com>
2023-05-13 20:14:35 -0300

    Creando los primeros test unitarios

--- src/test/java/com/sublimado/model/VentaTest.java
+++ src/test/java/com/sublimado/model/VentaTest.java
@@ -2,8 +2,6 @@ package com.sublimado.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 
